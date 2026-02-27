8bf1962d132b805acc533cb6fce310ac80d29ed0
fransstrom <frans.herrstrom@gmail.com>
2026-02-27 12:53:42 +0100

    fixed formatting

--- calc.c
+++ calc.c
@@ -1,17 +1,29 @@
 #include "calc.h"
 
-int add(int a, int b) { return a + b; }
+int add(int a, int b) {
+    return a + b;
+}
 
-int sub(int a, int b) { return a - b; }
+int sub(int a, int b) {
+    return a - b;
+}
 
-int mult(int a, int b) { return a * b; }
+int mult(int a, int b) {
+    return a * b;
+}
 
-float div(int a, int b) { return a / b; }
+float div(int a, int b) {
+    return a / b;
+}
 
-int mult_by_2(int a) { return a << 1; }
+int mult_by_2(int a) {
+    return a << 1;
+}
 
-int div_by_2_truncate(int a) { return a >> 1; }
+int div_by_2_truncate(int a) {
+    return a >> 1;
+}
 
-int remainder(int numerator, int denominator) {
+int remainder(int numerator, int denominator){
   return numerator % denominator;
 }
