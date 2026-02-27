4949689f9a88081dae4e343cd50c2b99a511bff1
Joakim Englund <joakimenglund@protonmail.com>
2026-02-27 08:41:14 +0100

    Fixed newlines in main

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 
 int main(void) {
     bool isRich = false;
-    printf("Hello Forsaken World!");
+    printf("Hello Forsaken World!\n");
 
     if (add(1, 1) == 2) {
         isRich = true;
     }
 
     if (isRich) {
-        printf("Squeeze every drop of value from your workers!");
+        printf("Squeeze every drop of value from your workers!\n");
     }
     else {
-        printf("Eat the rich!");
+        printf("Eat the rich!\n");
     }
     
     return 0;
