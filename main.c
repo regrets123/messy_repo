2bc5786fc22dc1e7050493a91e0917d2ae7302eb
fransstrom <frans.herrstrom@gmail.com>
2026-02-27 13:16:54 +0100

    1 comment with 1000 line changes

--- main.c
+++ main.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include "calc.h"
+#include <stdbool.h>
+#include <stdio.h>
 #define TWO 2
-// According to all known laws of aviation, there is no way a bee should be able to fly.
-// Its wings are too small to get its fat little body off the ground.
-// The bee, of course, flies anyway because bees don't care what humans think is impossible.
-void println(char* msg) {
-    printf(msg);
-    printf("\n");
+// According to all known laws of aviation, there is no way a bee should be able
+// to fly. Its wings are too small to get its fat little body off the ground.
+// The bee, of course, flies anyway because bees don't care what humans think is
+// impossible.
+void println(char *msg) {
+  printf(msg);
+  printf("\n");
 }
 
-
 int main(void) {
-    bool isRich = false;
-    printf("Hello Forsaken World!\n");
+  bool isRich = false;
+  printf("Hello Forsaken World!\n");
 
-    if (add(1, 1) == 2) {
-        isRich = true;
-    }
+  if (add(1, 1) == 2) {
+    isRich = true;
+  }
 
-    if (isRich) {
-        printf("Squeeze every drop of value from your workers!\n");
-    }
-    else {
-        printf("Let's escape to the one place that hasn't been corrupted by capitalism... SPACE!\n");
-    }
+  if (isRich) {
+    printf("Squeeze every drop of value from your workers!\n");
+  } else {
+    printf("Let's escape to the one place that hasn't been corrupted by "
+           "capitalism... SPACE!\n");
+  }
 
-    return 0;
+  return 0;
 }
