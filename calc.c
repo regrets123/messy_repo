348c8a87933433fbc6f3868d585303d3de65730c
Joakim Englund <joakimenglund@protonmail.com>
2026-02-27 08:35:14 +0100

    Added mult/div by 2

--- calc.c
+++ calc.c
@@ -3,3 +3,11 @@
 int add(int a, int b) {
     return a + b;
 }
+
+int mult_by_2(int a) {
+    return a << 1;
+}
+
+int div_by_2_truncate(int a) {
+    return a >> 1;
+}
--- calc.h
+++ calc.h
@@ -2,5 +2,7 @@
 #define CALC_H_
 
 int add(int a, int b);
+int mult_by_2(int a);
+int div_by_2_truncate(int a);
 
 #endif
